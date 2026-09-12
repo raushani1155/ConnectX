05f8f6f002f399c99a2a30a635b712cbfd6325b9
raushani1155 <raushaniraj308@gmail.com>
2026-09-12 16:48:20 +0530

    DEPOLYING FRONTEND

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,3 +1,8 @@
-const server = "http://localhost:8000";
+let IS_PROD = true;
+const server = IS_PROD ?
+    "https://connectx1.onrender.com" :
+
+    "http://localhost:8000"
+
 
 export default server;
